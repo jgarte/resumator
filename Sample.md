181c0f19b36233dbfb4464ac6d551a0a2836d763
calvang <calvin.huang5@gmail.com>
2020-07-21 18:17:08 -0400

    Add markdown parser and pdf output

---
+++ Sample.md
@@ -0,0 +1,53 @@
+# <Your Name Here>
+
+## Education
+
+### University of Python
+
+- BSE Computer Science
+- GPA: 4.0
+- Course Highlights: Python 404, Jinja 281
+- LOCATION: City, State
+- DATES: date1-date2
+
+## Experience
+
+### Project 1
+
+- What you did
+- LOCATION: City, State
+- DATES: date1-date2
+
+### Project 2
+
+- What you did
+- LOCATION: City, State
+- DATES: date1-date2
+
+### Project 3
+
+- What you did
+- LOCATION: City, State
+- DATES: date1-date2
+
+### Project 4
+
+- What you did
+- LOCATION: City, State
+- DATES: date1-date2
+
+## Skills
+
+### Programming Languages
+
+- Python
+
+### Markup
+
+- JSON
+- Markdown
+
+## Honors
+
+- Best Python programmer self-certified
+
--- resumator.py
+++ resumator.py
@@ -1,10 +1,32 @@
 import sys
 import os
 import getopt
+from datetime import datetime
 import jinja2
 from jinja2 import Template
+import mistune
 
-def resumator(in_file):
+def parse_markdown(in_file):
+    '''Read resume data from markdown file'''
+    with open(in_file, 'r') as file:
+        file_text = file.read();
+        file_html = mistune.html(file_text)
+    print(file_html)
+        
+def output_to_tex(filename, data):
+    '''Write resume to new tex file'''
+    now = datetime.now().strftime('-%Y-%d-%m_%H:%M:%S')
+    full_filename = filename + now + '.tex'
+    with open(os.path.abspath(os.path.join('latex',full_filename)), 'w') as output:
+        output.write(data)
+    return full_filename
+
+def output_to_pdf(filename):
+    '''Print resume to new pdf file'''
+    os.system('pdflatex latex/' + filename)
+
+def resumator(in_file, data):
+    '''Fill Latex resume template'''
     latex_env = jinja2.Environment(
         block_start_string = '\BLOCK{',
         block_end_string = '}',
@@ -16,10 +38,13 @@ def resumator(in_file):
         line_comment_prefix = '%#',
         trim_blocks = True,
         autoescape = False,
-        loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+        loader = jinja2.FileSystemLoader(os.path.abspath('templates/'))
     )
     template = latex_env.get_template('template1.tex')
-    print(template.render(section1='Long Form', section2='Short Form'))
+    renderer = template.render(**data)
+    # print(renderer)
+    tex_out = output_to_tex('Resume',renderer)
+    output_to_pdf(tex_out)
 
 def main():
     # read and process command line args
@@ -37,21 +62,13 @@ def main():
             sys.exit(2)
         else:
             in_file = arg
-    latex_env = jinja2.Environment(
-        block_start_string = '\BLOCK{',
-        block_end_string = '}',
-        variable_start_string = '\VAR{',
-        variable_end_string = '}',
-        comment_start_string = '\#{',
-        comment_end_string = '}',
-        line_statement_prefix = '%%',
-        line_comment_prefix = '%#',
-        trim_blocks = True,
-        autoescape = False,
-        loader = jinja2.FileSystemLoader(os.path.abspath('.'))
-    )
-    template = latex_env.get_template('template1.tex')
-    print(template.render())
+    data = {
+        'name':'Calvin Huang',
+        'cell':'(513) 693-5266)',
+        'var':1
+    }
+    resumator(in_file, data)
+    parse_markdown('Sample.md')
 
 if __name__ == '__main__':
     main()
